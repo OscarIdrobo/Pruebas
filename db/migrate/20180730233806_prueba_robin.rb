906b96317c766820d86958c89a5e2009be90dd59
jhondavid07 <jdavidducuara@gmail.com>
2018-07-30 19:07:08 -0500

    Creacion migracion tabla usuarios

---
+++ db/migrate/20180730233806_prueba_robin.rb
@@ -0,0 +1,4 @@
+class PruebaRobin < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ db/migrate/20180730235933_usuarios.rb
@@ -0,0 +1,20 @@
+class Usuarios < ActiveRecord::Migration[5.2]
+  def change
+
+    create_table :usuarios do |t|
+      t.integer :idusuarios
+      t.string nombre_usuario
+      t.string apellido_usuario
+      t.string tipo_documento
+      t.string documento character
+      t.string cel_personal
+      t.string correo_personal character
+      t.string contrasena character
+      t.string direccion_residencia
+      t.string latitud_dir_residencia
+      t.string longitud_dir_encargado
+      t.timestamp fecha_creacion
+      t.string estado estado
+      t.string rol rol
+     end
+end
