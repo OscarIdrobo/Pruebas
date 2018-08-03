06fb456a048eb6a9ffb1a24fcdef6b918b907a12
jhondavid07 <jdavidducuara@gmail.com>
2018-08-03 18:08:27 -0500

    Actualizacion Relaciones de Tablas

--- db/migrate/20180803032202_create_administradors.rb
+++
@@ -1,8 +0,0 @@
-class CreateAdministradors < ActiveRecord::Migration[5.2]
-  def change
-    create_table :administradors do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20180803035151_uno_muchos_servidor_direccionip.rb
+++
@@ -1,5 +0,0 @@
-class UnoMuchosServidorDireccionip < ActiveRecord::Migration[5.2]
-  def change
-    add_column("direccion_ips","servidor_id","string")
-  end
-end
